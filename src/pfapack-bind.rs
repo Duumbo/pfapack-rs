9601adbb52b47cef0e31ddd174e59555afbca8c8
BCarnaval <antoinedelagrave@hotmail.com>
2023-06-05 17:01:36 -0400

    Partial documentation
    -> skbpf10
    -> skbpfa
    -> skbptrd

--- src/pfapack-bind.rs
+++ src/pfapack-bind.rs
@@ -430,326 +430,92 @@ pub unsafe fn zskpf10(
     )
 }
 
-#[inline]
-pub unsafe fn sskbtrd(
-    arg1: u8,
-    arg2: u8,
-    arg3: u8,
-    arg4: &[i32],
-    arg5: &[i32],
-    arg6: &mut [f32],
-    arg7: &[i32],
-    arg8: &mut [f32],
-    arg9: &mut [f32],
-    arg10: &[i32],
-    arg11: &mut [f32],
-    arg12: &mut [i32],
-) {
-    ffi::sskbtrd_(
-        &(arg1 as c_char),
-        &(arg2 as c_char),
-        &(arg3 as c_char),
-        arg4.as_ptr(),
-        arg5.as_ptr(),
-        arg6.as_mut_ptr(),
-        arg7.as_ptr(),
-        arg8.as_mut_ptr(),
-        arg9.as_mut_ptr(),
-        arg10.as_ptr(),
-        arg11.as_mut_ptr(),
-        arg12.as_mut_ptr(),
-    )
-}
+/// Reduces a complex skew-symmetric band matrix A to real
+/// skew-symmetric tridiagonal form T by a unitary congruence transformation.
+///
+/// # Purpose
+///
+/// `zskbtrd` reduces a complex skew-symmetric band matrix A to real
+/// skew-symmetric tridiagonal form T by a unitary congruence transformation:
+/// Q^dagger * A * Q^* = T. Alternatively, the routine can also compute a
+/// partial tridiagonal form useful for computing the Pfaffian.
+///
+/// # Further Details
+///
+/// The storage scheme for the skew-symmetric matrix is identical to the
+/// storage scheme for symmetric or Hermitian band matrices in LAPACK,
+/// i.e. the diagonal and the `kd` super- or subdiagonals are stored in an
+/// array with `kd+1` rows and `n` columns. Note that the zero diagonal must
+/// also be explicitely stored (this was done to keep the structure of
+/// the program identical to the symmetric case)
+///
+/// In particular this means that if
+/// - `uplo = 'U'`, then `ab(kd+1+i-j,j)` = A(i,j) for `max(1,j-kd)<=i<=j`
+///
+///   Example: `n=5`, `kd=2`
+///
+/// (  0     a12   a13              )
+/// (  -a12  0     a23   a24        )
+/// (  -a13  -a23  0     a34   a35  )
+/// (        -a24  -a34  0     a45  )
+/// (              -a35  -a45  0    )
+///
+/// is stored as
+///
+///  x   x   a13 a24 a35
+///  x   a12 a23 a34 a45
+///  0   0   0   0   0
+///
+/// where x denotes an unused entry
+///
+/// - `uplo = 'L'`, then `ab(1+i-j,j)` = A(i,j) for `j<=i<=min(n,j+kd)`
+///
+///   Example: `n=5`, `kd=2`
+///
+/// (  0     -a21  -a31              )
+/// (  a21   0     -a32  -a42        )
+/// (  a31   a32   0     -a43  -a53  )
+/// (        a42   a43   0     -a54  )
+/// (              a53   a54   0     )
+///
+/// is stored as
+///
+///  0   0   0   0   0
+///  a21 a32 a43 a54 x
+///  a31 a42 a53 x   x
+///
+/// where x denotes an unused entry
+pub mod skbtrd;
 
-#[inline]
-pub unsafe fn dskbtrd(
-    arg1: u8,
-    arg2: u8,
-    arg3: u8,
-    arg4: &[i32],
-    arg5: &[i32],
-    arg6: &mut [f64],
-    arg7: &[i32],
-    arg8: &mut [f64],
-    arg9: &mut [f64],
-    arg10: &[i32],
-    arg11: &mut [f64],
-    arg12: &mut [i32],
-) {
-    ffi::dskbtrd_(
-        &(arg1 as c_char),
-        &(arg2 as c_char),
-        &(arg3 as c_char),
-        arg4.as_ptr(),
-        arg5.as_ptr(),
-        arg6.as_mut_ptr(),
-        arg7.as_ptr(),
-        arg8.as_mut_ptr(),
-        arg9.as_mut_ptr(),
-        arg10.as_ptr(),
-        arg11.as_mut_ptr(),
-        arg12.as_mut_ptr(),
-    )
-}
+/// Computes the Pfaffian of a banded skew-symmetric matrix.
+///
+/// # Purpose
+///
+/// `skbpfa` computes the Pfaffian of a banded skew-symmetric matrix.
+///
+/// # Further details
+///
+/// For odd-sized matrices, the Pfaffian is 0 by default, hence no computation
+/// is needed in this case. For even-sized matrices, the Pfaffian is computed
+/// by bringing the skew-symmetric matrix A into tridiagonal form T by a
+/// unitary congruence transformation: `Q^H * A * Q^* = T`. This transformation
+/// is computed by the routine [`ssktrd`] (for further details see there).
+pub mod skbpfa;
 
-#[inline]
-pub unsafe fn cskbtrd(
-    arg1: u8,
-    arg2: u8,
-    arg3: u8,
-    arg4: &[i32],
-    arg5: &[i32],
-    arg6: &mut [c32],
-    arg7: &[i32],
-    arg8: &mut [f32],
-    arg9: &mut [c32],
-    arg10: &mut [c32],
-    arg11: &[i32],
-    arg12: &mut [c32],
-    arg13: &mut [f32],
-    arg14: &mut [i32],
-) {
-    ffi::cskbtrd_(
-        &(arg1 as c_char),
-        &(arg2 as c_char),
-        &(arg3 as c_char),
-        arg4.as_ptr(),
-        arg5.as_ptr(),
-        arg6.as_mut_ptr() as *mut _,
-        arg7.as_ptr(),
-        arg8.as_mut_ptr(),
-        arg9.as_mut_ptr() as *mut _,
-        arg10.as_mut_ptr() as *mut _,
-        arg11.as_ptr(),
-        arg12.as_mut_ptr() as *mut _,
-        arg13.as_mut_ptr(),
-        arg14.as_mut_ptr(),
-    )
-}
-
-#[inline]
-pub unsafe fn zskbtrd(
-    arg1: u8,
-    arg2: u8,
-    arg3: u8,
-    arg4: &[i32],
-    arg5: &[i32],
-    arg6: &mut [c64],
-    arg7: &[i32],
-    arg8: &mut [f64],
-    arg9: &mut [c64],
-    arg10: &mut [c64],
-    arg11: &[i32],
-    arg12: &mut [c64],
-    arg13: &mut [f64],
-    arg14: &mut [i32],
-) {
-    ffi::zskbtrd_(
-        &(arg1 as c_char),
-        &(arg2 as c_char),
-        &(arg3 as c_char),
-        arg4.as_ptr(),
-        arg5.as_ptr(),
-        arg6.as_mut_ptr() as *mut _,
-        arg7.as_ptr(),
-        arg8.as_mut_ptr(),
-        arg9.as_mut_ptr() as *mut _,
-        arg10.as_mut_ptr() as *mut _,
-        arg11.as_ptr(),
-        arg12.as_mut_ptr() as *mut _,
-        arg13.as_mut_ptr(),
-        arg14.as_mut_ptr(),
-    )
-}
-
-#[inline]
-pub unsafe fn sskbpfa(
-    arg1: u8,
-    arg2: &[i32],
-    arg3: &[i32],
-    arg4: &mut [f32],
-    arg5: &[i32],
-    arg6: &mut [f32],
-    arg7: &mut [f32],
-    arg8: &mut [i32],
-) {
-    ffi::sskbpfa_(
-        &(arg1 as c_char),
-        arg2.as_ptr(),
-        arg3.as_ptr(),
-        arg4.as_mut_ptr(),
-        arg5.as_ptr(),
-        arg6.as_mut_ptr(),
-        arg7.as_mut_ptr(),
-        arg8.as_mut_ptr(),
-    )
-}
-
-#[inline]
-pub unsafe fn dskbpfa(
-    arg1: u8,
-    arg2: &[i32],
-    arg3: &[i32],
-    arg4: &mut [f64],
-    arg5: &[i32],
-    arg6: &mut [f64],
-    arg7: &mut [f64],
-    arg8: &mut [i32],
-) {
-    ffi::dskbpfa_(
-        &(arg1 as c_char),
-        arg2.as_ptr(),
-        arg3.as_ptr(),
-        arg4.as_mut_ptr(),
-        arg5.as_ptr(),
-        arg6.as_mut_ptr(),
-        arg7.as_mut_ptr(),
-        arg8.as_mut_ptr(),
-    )
-}
-
-#[inline]
-pub unsafe fn cskbpfa(
-    arg1: u8,
-    arg2: &[i32],
-    arg3: &[i32],
-    arg4: &mut [c32],
-    arg5: &[i32],
-    arg6: &mut [c32],
-    arg7: &mut [c32],
-    arg8: &mut [f32],
-    arg9: &mut [i32],
-) {
-    ffi::cskbpfa_(
-        &(arg1 as c_char),
-        arg2.as_ptr(),
-        arg3.as_ptr(),
-        arg4.as_mut_ptr() as *mut _,
-        arg5.as_ptr(),
-        arg6.as_mut_ptr() as *mut _,
-        arg7.as_mut_ptr() as *mut _,
-        arg8.as_mut_ptr(),
-        arg9.as_mut_ptr(),
-    )
-}
-
-#[inline]
-pub unsafe fn zskbpfa(
-    arg1: u8,
-    arg2: &[i32],
-    arg3: &[i32],
-    arg4: &mut [c64],
-    arg5: &[i32],
-    arg6: &mut [c64],
-    arg7: &mut [c64],
-    arg8: &mut [f64],
-    arg9: &mut [i32],
-) {
-    ffi::zskbpfa_(
-        &(arg1 as c_char),
-        arg2.as_ptr(),
-        arg3.as_ptr(),
-        arg4.as_mut_ptr() as *mut _,
-        arg5.as_ptr(),
-        arg6.as_mut_ptr() as *mut _,
-        arg7.as_mut_ptr() as *mut _,
-        arg8.as_mut_ptr(),
-        arg9.as_mut_ptr(),
-    )
-}
-
-#[inline]
-pub unsafe fn sskbpf10(
-    arg1: u8,
-    arg2: &[i32],
-    arg3: &[i32],
-    arg4: &mut [f32],
-    arg5: &[i32],
-    arg6: &mut [f32],
-    arg7: &mut [f32],
-    arg8: &mut [i32],
-) {
-    ffi::sskbpf10_(
-        &(arg1 as c_char),
-        arg2.as_ptr(),
-        arg3.as_ptr(),
-        arg4.as_mut_ptr(),
-        arg5.as_ptr(),
-        arg6.as_mut_ptr(),
-        arg7.as_mut_ptr(),
-        arg8.as_mut_ptr(),
-    )
-}
-
-#[inline]
-pub unsafe fn dskbpf10(
-    arg1: u8,
-    arg2: &[i32],
-    arg3: &[i32],
-    arg4: &mut [f64],
-    arg5: &[i32],
-    arg6: &mut [f64],
-    arg7: &mut [f64],
-    arg8: &mut [i32],
-) {
-    ffi::dskbpf10_(
-        &(arg1 as c_char),
-        arg2.as_ptr(),
-        arg3.as_ptr(),
-        arg4.as_mut_ptr(),
-        arg5.as_ptr(),
-        arg6.as_mut_ptr(),
-        arg7.as_mut_ptr(),
-        arg8.as_mut_ptr(),
-    )
-}
-
-#[inline]
-pub unsafe fn cskbpf10(
-    arg1: u8,
-    arg2: &[i32],
-    arg3: &[i32],
-    arg4: &mut [c32],
-    arg5: &[i32],
-    arg6: &mut [c32],
-    arg7: &mut [c32],
-    arg8: &mut [f32],
-    arg9: &mut [i32],
-) {
-    ffi::cskbpf10_(
-        &(arg1 as c_char),
-        arg2.as_ptr(),
-        arg3.as_ptr(),
-        arg4.as_mut_ptr() as *mut _,
-        arg5.as_ptr(),
-        arg6.as_mut_ptr() as *mut _,
-        arg7.as_mut_ptr() as *mut _,
-        arg8.as_mut_ptr(),
-        arg9.as_mut_ptr(),
-    )
-}
-
-#[inline]
-pub unsafe fn zskbpf10(
-    arg1: u8,
-    arg2: &[i32],
-    arg3: &[i32],
-    arg4: &mut [c64],
-    arg5: &[i32],
-    arg6: &mut [c64],
-    arg7: &mut [c64],
-    arg8: &mut [f64],
-    arg9: &mut [i32],
-) {
-    ffi::zskbpf10_(
-        &(arg1 as c_char),
-        arg2.as_ptr(),
-        arg3.as_ptr(),
-        arg4.as_mut_ptr() as *mut _,
-        arg5.as_ptr(),
-        arg6.as_mut_ptr() as *mut _,
-        arg7.as_mut_ptr() as *mut _,
-        arg8.as_mut_ptr(),
-        arg9.as_mut_ptr(),
-    )
-}
+/// Computes the Pfaffian of a banded skew-symmetric matrix taking special care
+/// to avoid numerical under- or overflow.
+///
+/// # Purpose
+///
+/// `skbpf10` computes the Pfaffian of a banded skew-symmetric matrix, taking
+/// special care to avoid numerical under- or overflow. (at the cost of
+/// possible additional round-off errors).
+///
+/// # Further details
+///
+/// For odd-sized matrices, the Pfaffian is 0 by default, hence no computation
+/// is needed in this case. For even-sized matrices, the Pfaffian is computed
+/// by bringing the skew-symmetric matrix A into tridiagonal form T by a
+/// unitary congruence transformation: Q^H * A * Q^* = T. This transformation
+/// is computed by the routine [`ssktrd`] (for further details see there).
+pub mod skbpf10;
---
+++ src/skbtrd.rs
@@ -0,0 +1,363 @@
+use crate::{c32, c64, c_char};
+
+/// Single precision real.
+///
+/// # Arguments
+///
+/// __*`vect`*__ - (input) If `vect= 'N'`: do not form Q; If `vect = 'V':
+/// form Q; if `vect = 'U': update a matrix X, by forming X*Q.
+/// * __*`uplo`*__ - (input) If `uplo = 'U'`: Upper triangle of A is stored;
+/// if `uplo = 'L'`: Lower triangle of A is stored.
+/// __*`mode`*__ - (input) If `mode = 'N'`: A is fully triagonalized; if
+/// `mode = 'P'`: A is partially triagonalized for Pfaffian computation
+/// (details see below).
+/// __*`n`*__ - (input) The order of the matrix A. `n >= 0`. `n` must be even if
+/// `mode = 'P'`.
+/// * __*`kd`*__ - (input) Number of super- (if `uplo = 'U'`) or subdiagonals
+/// (if `uplo = 'L'`). `kd >= 0`.
+/// * __*`ab`*__ - (input/output) Dimension (`ldab, n`) On entry, the upper or
+/// lower triangle of the skew-symmetric band matrix A, stored in the first
+/// `kd+1` rows of the array. The j-th column of A is stored in the j-th column
+/// of the array `ab` as follows:
+///     > if `uplo = 'U'`, `ab(kd+1+i-j,j)` = A(i,j) for `max(1,j-kd)<=i<=j`;
+///     > if `uplo = 'L'`, `ab(1+i-j,j)` = A(i,j) for `j<=i<=min(n,j+kd)`.
+/// On exit, the zero diagonal elements of AB are left unchanged, if `kd > 0`,
+/// the elements on the first superdiagonal (if `uplo = 'U'`) or the first
+/// subdiagonal (if UPLO = 'L') are overwritten by the off-diagonal elements of
+/// T; the rest of AB is overwritten by values generated during the reduction.
+/// If `mode = 'P'`, only the off-diagonal entries in the odd rows (columns)
+/// are computed for `uplo = 'U'` (`uplo = 'L'`).
+/// * __*`ldab`*__ (input) The leading dimension of the array `ab`.
+/// `ldab >= kd+1`.
+/// * __*`e`*__ - (output) Dimension (`n-1`). The off-diagonal elements of the
+/// tridiagonal matrix T: `e(i)` = T(i,i+1) if `uplo = 'U'`; `e(i)` = T(i+1,i)
+/// if `uplo = 'L'`.
+/// * __*`q`*__ - (input/output) Dimension (`ldq, n`). On entry, if `vect =`
+///
+///     > 'U', then Q must contain an `n`-by-`n` matrix X;
+///
+///     > 'N' or 'V', then Q need not be set.
+///
+/// On exit, it `vect =`
+///
+///     > 'V', Q contains the N-by-N unitary matrix Q;
+///
+///     > 'U', Q contains the product X*Q;
+///
+///     > 'N' , the array Q is not referenced.
+/// __*`ldq`*__ - (input) The leading dimension of the array Q. `ldq >= 1`, and
+/// `ldq >= n` if `vect = 'V' or 'U'`.
+/// * __*`work`*__ - (workspace) Array, dimension (`n`).
+/// * __*`info`*__ - (output)
+///
+///     > if = 0: successful exit;
+///
+///     > if < 0: if the return value is -i, the i-th argument had an illegal
+///     > value;
+///
+///     > if = -100: failed to allocate enough internal memory.
+#[inline]
+pub unsafe fn sskbtrd(
+    vect: u8,
+    uplo: u8,
+    mode: u8,
+    n: &i32,
+    kd: &i32,
+    ab: &mut [f32],
+    ldab: &i32,
+    e: &mut [f32],
+    q: &mut [f32],
+    ldq: &i32,
+    work: &mut [f32],
+    info: &mut i32,
+) {
+    ffi::sskbtrd_(
+        &(vect as c_char),
+        &(uplo as c_char),
+        &(mode as c_char),
+        n,
+        kd,
+        ab.as_mut_ptr(),
+        ldab,
+        e.as_mut_ptr(),
+        q.as_mut_ptr(),
+        ldq,
+        work.as_mut_ptr(),
+        info,
+    )
+}
+
+/// Double precision real.
+///
+/// # Arguments
+///
+/// __*`vect`*__ - (input) If `vect= 'N'`: do not form Q; If `vect = 'V':
+/// form Q; if `vect = 'U': update a matrix X, by forming X*Q.
+/// * __*`uplo`*__ - (input) If `uplo = 'U'`: Upper triangle of A is stored;
+/// if `uplo = 'L'`: Lower triangle of A is stored.
+/// __*`mode`*__ - (input) If `mode = 'N'`: A is fully triagonalized; if
+/// `mode = 'P'`: A is partially triagonalized for Pfaffian computation
+/// (details see below).
+/// __*`n`*__ - (input) The order of the matrix A. `n >= 0`. `n` must be even if
+/// `mode = 'P'`.
+/// * __*`kd`*__ - (input) Number of super- (if `uplo = 'U'`) or subdiagonals
+/// (if `uplo = 'L'`). `kd >= 0`.
+/// * __*`ab`*__ - (input/output) Dimension (`ldab, n`) On entry, the upper or
+/// lower triangle of the skew-symmetric band matrix A, stored in the first
+/// `kd+1` rows of the array. The j-th column of A is stored in the j-th column
+/// of the array `ab` as follows:
+///     > if `uplo = 'U'`, `ab(kd+1+i-j,j)` = A(i,j) for `max(1,j-kd)<=i<=j`;
+///     > if `uplo = 'L'`, `ab(1+i-j,j)` = A(i,j) for `j<=i<=min(n,j+kd)`.
+/// On exit, the zero diagonal elements of AB are left unchanged, if `kd > 0`,
+/// the elements on the first superdiagonal (if `uplo = 'U'`) or the first
+/// subdiagonal (if UPLO = 'L') are overwritten by the off-diagonal elements of
+/// T; the rest of AB is overwritten by values generated during the reduction.
+/// If `mode = 'P'`, only the off-diagonal entries in the odd rows (columns)
+/// are computed for `uplo = 'U'` (`uplo = 'L'`).
+/// * __*`ldab`*__ (input) The leading dimension of the array `ab`.
+/// `ldab >= kd+1`.
+/// * __*`e`*__ - (output) Dimension (`n-1`). The off-diagonal elements of the
+/// tridiagonal matrix T: `e(i)` = T(i,i+1) if `uplo = 'U'`; `e(i)` = T(i+1,i)
+/// if `uplo = 'L'`.
+/// * __*`q`*__ - (input/output) Dimension (`ldq, n`). On entry, if `vect =`
+///
+///     > 'U', then Q must contain an `n`-by-`n` matrix X;
+///
+///     > 'N' or 'V', then Q need not be set.
+///
+/// On exit, it `vect =`
+///
+///     > 'V', Q contains the N-by-N unitary matrix Q;
+///
+///     > 'U', Q contains the product X*Q;
+///
+///     > 'N' , the array Q is not referenced.
+/// __*`ldq`*__ - (input) The leading dimension of the array Q. `ldq >= 1`, and
+/// `ldq >= n` if `vect = 'V' or 'U'`.
+/// * __*`work`*__ - (workspace) Array, dimension (`n`).
+/// * __*`info`*__ - (output)
+///
+///     > if = 0: successful exit;
+///
+///     > if < 0: if the return value is -i, the i-th argument had an illegal
+///     > value;
+///
+///     > if = -100: failed to allocate enough internal memory.
+#[inline]
+pub unsafe fn dskbtrd(
+    vect: u8,
+    uplo: u8,
+    mode: u8,
+    n: &i32,
+    kd: &i32,
+    ab: &mut [f64],
+    ldab: &i32,
+    e: &mut [f64],
+    q: &mut [f64],
+    ldq: &i32,
+    work: &mut [f64],
+    info: &mut i32,
+) {
+    ffi::dskbtrd_(
+        &(vect as c_char),
+        &(uplo as c_char),
+        &(mode as c_char),
+        n,
+        kd,
+        ab.as_mut_ptr(),
+        ldab,
+        e.as_mut_ptr(),
+        q.as_mut_ptr(),
+        ldq,
+        work.as_mut_ptr(),
+        info,
+    )
+}
+
+/// Single precision complex.
+///
+/// # Arguments
+///
+/// __*`vect`*__ - (input) If `vect= 'N'`: do not form Q; If `vect = 'V':
+/// form Q; if `vect = 'U': update a matrix X, by forming X*Q.
+/// * __*`uplo`*__ - (input) If `uplo = 'U'`: Upper triangle of A is stored;
+/// if `uplo = 'L'`: Lower triangle of A is stored.
+/// __*`mode`*__ - (input) If `mode = 'N'`: A is fully triagonalized; if
+/// `mode = 'P'`: A is partially triagonalized for Pfaffian computation
+/// (details see below).
+/// __*`n`*__ - (input) The order of the matrix A. `n >= 0`. `n` must be even if
+/// `mode = 'P'`.
+/// * __*`kd`*__ - (input) Number of super- (if `uplo = 'U'`) or subdiagonals
+/// (if `uplo = 'L'`). `kd >= 0`.
+/// * __*`ab`*__ - (input/output) Dimension (`ldab, n`) On entry, the upper or
+/// lower triangle of the skew-symmetric band matrix A, stored in the first
+/// `kd+1` rows of the array. The j-th column of A is stored in the j-th column
+/// of the array `ab` as follows:
+///     > if `uplo = 'U'`, `ab(kd+1+i-j,j)` = A(i,j) for `max(1,j-kd)<=i<=j`;
+///     > if `uplo = 'L'`, `ab(1+i-j,j)` = A(i,j) for `j<=i<=min(n,j+kd)`.
+/// On exit, the zero diagonal elements of AB are left unchanged, if `kd > 0`,
+/// the elements on the first superdiagonal (if `uplo = 'U'`) or the first
+/// subdiagonal (if UPLO = 'L') are overwritten by the off-diagonal elements of
+/// T; the rest of AB is overwritten by values generated during the reduction.
+/// If `mode = 'P'`, only the off-diagonal entries in the odd rows (columns)
+/// are computed for `uplo = 'U'` (`uplo = 'L'`).
+/// * __*`ldab`*__ (input) The leading dimension of the array `ab`.
+/// `ldab >= kd+1`.
+/// * __*`e`*__ - (output) Dimension (`n-1`). The off-diagonal elements of the
+/// tridiagonal matrix T: `e(i)` = T(i,i+1) if `uplo = 'U'`; `e(i)` = T(i+1,i)
+/// if `uplo = 'L'`.
+/// * __*`detq`*__ - (output) The value of the determinant of Q, which is a pure
+/// phase factor. Always computed, even if Q is not explicitely formed.
+/// * __*`q`*__ - (input/output) Dimension (`ldq, n`). On entry, if `vect =`
+///
+///     > 'U', then Q must contain an `n`-by-`n` matrix X;
+///
+///     > 'N' or 'V', then Q need not be set.
+///
+/// On exit, it `vect =`
+///
+///     > 'V', Q contains the N-by-N unitary matrix Q;
+///
+///     > 'U', Q contains the product X*Q;
+///
+///     > 'N' , the array Q is not referenced.
+/// __*`ldq`*__ - (input) The leading dimension of the array Q. `ldq >= 1`, and
+/// `ldq >= n` if `vect = 'V' or 'U'`.
+/// * __*`work`*__ - (workspace) Array, dimension (`n`).
+/// * __*`rwork`*__ - (workspace) Array, dimension (`n`).
+/// * __*`info`*__ - (output)
+///
+///     > if = 0: successful exit;
+///
+///     > if < 0: if the return value is -i, the i-th argument had an illegal
+///     > value;
+///
+///     > if = -100: failed to allocate enough internal memory.
+#[inline]
+pub unsafe fn cskbtrd(
+    vect: u8,
+    uplo: u8,
+    mode: u8,
+    n: &i32,
+    kd: &i32,
+    ab: &mut [c32],
+    ldab: &[i32],
+    e: &mut [f32],
+    detq: &mut [c32],
+    q: &mut [c32],
+    ldq: &i32,
+    work: &mut [c32],
+    rwork: &mut [f32],
+    info: &mut i32,
+) {
+    ffi::cskbtrd_(
+        &(vect as c_char),
+        &(uplo as c_char),
+        &(mode as c_char),
+        n,
+        kd,
+        ab.as_mut_ptr() as *mut _,
+        ldab.as_ptr(),
+        e.as_mut_ptr(),
+        detq.as_mut_ptr() as *mut _,
+        q.as_mut_ptr() as *mut _,
+        ldq,
+        work.as_mut_ptr() as *mut _,
+        rwork.as_mut_ptr(),
+        info,
+    )
+}
+
+/// Double precision complex.
+///
+/// # Arguments
+///
+/// __*`vect`*__ - (input) If `vect= 'N'`: do not form Q; If `vect = 'V':
+/// form Q; if `vect = 'U': update a matrix X, by forming X*Q.
+/// * __*`uplo`*__ - (input) If `uplo = 'U'`: Upper triangle of A is stored;
+/// if `uplo = 'L'`: Lower triangle of A is stored.
+/// __*`mode`*__ - (input) If `mode = 'N'`: A is fully triagonalized; if
+/// `mode = 'P'`: A is partially triagonalized for Pfaffian computation
+/// (details see below).
+/// __*`n`*__ - (input) The order of the matrix A. `n >= 0`. `n` must be even if
+/// `mode = 'P'`.
+/// * __*`kd`*__ - (input) Number of super- (if `uplo = 'U'`) or subdiagonals
+/// (if `uplo = 'L'`). `kd >= 0`.
+/// * __*`ab`*__ - (input/output) Dimension (`ldab, n`) On entry, the upper or
+/// lower triangle of the skew-symmetric band matrix A, stored in the first
+/// `kd+1` rows of the array. The j-th column of A is stored in the j-th column
+/// of the array `ab` as follows:
+///     > if `uplo = 'U'`, `ab(kd+1+i-j,j)` = A(i,j) for `max(1,j-kd)<=i<=j`;
+///     > if `uplo = 'L'`, `ab(1+i-j,j)` = A(i,j) for `j<=i<=min(n,j+kd)`.
+/// On exit, the zero diagonal elements of AB are left unchanged, if `kd > 0`,
+/// the elements on the first superdiagonal (if `uplo = 'U'`) or the first
+/// subdiagonal (if UPLO = 'L') are overwritten by the off-diagonal elements of
+/// T; the rest of AB is overwritten by values generated during the reduction.
+/// If `mode = 'P'`, only the off-diagonal entries in the odd rows (columns)
+/// are computed for `uplo = 'U'` (`uplo = 'L'`).
+/// * __*`ldab`*__ (input) The leading dimension of the array `ab`.
+/// `ldab >= kd+1`.
+/// * __*`e`*__ - (output) Dimension (`n-1`). The off-diagonal elements of the
+/// tridiagonal matrix T: `e(i)` = T(i,i+1) if `uplo = 'U'`; `e(i)` = T(i+1,i)
+/// if `uplo = 'L'`.
+/// * __*`detq`*__ - (output) The value of the determinant of Q, which is a pure
+/// phase factor. Always computed, even if Q is not explicitely formed.
+/// * __*`q`*__ - (input/output) Dimension (`ldq, n`). On entry, if `vect =`
+///
+///     > 'U', then Q must contain an `n`-by-`n` matrix X;
+///
+///     > 'N' or 'V', then Q need not be set.
+///
+/// On exit, it `vect =`
+///
+///     > 'V', Q contains the N-by-N unitary matrix Q;
+///
+///     > 'U', Q contains the product X*Q;
+///
+///     > 'N' , the array Q is not referenced.
+/// __*`ldq`*__ - (input) The leading dimension of the array Q. `ldq >= 1`, and
+/// `ldq >= n` if `vect = 'V' or 'U'`.
+/// * __*`work`*__ - (workspace) Array, dimension (`n`).
+/// * __*`rwork`*__ - (workspace) Array, dimension (`n`).
+/// * __*`info`*__ - (output)
+///
+///     > if = 0: successful exit;
+///
+///     > if < 0: if the return value is -i, the i-th argument had an illegal
+///     > value;
+///
+///     > if = -100: failed to allocate enough internal memory.
+#[inline]
+pub unsafe fn zskbtrd(
+    vect: u8,
+    uplo: u8,
+    mode: u8,
+    n: &i32,
+    kd: &i32,
+    a: &mut [c64],
+    ldab: &i32,
+    e: &mut [f64],
+    detq: &mut [c64],
+    q: &mut [c64],
+    ldq: &i32,
+    work: &mut [c64],
+    rwork: &mut [f64],
+    info: &mut i32,
+) {
+    ffi::zskbtrd_(
+        &(vect as c_char),
+        &(uplo as c_char),
+        &(mode as c_char),
+        n,
+        kd,
+        a.as_mut_ptr() as *mut _,
+        ldab,
+        e.as_mut_ptr(),
+        detq.as_mut_ptr() as *mut _,
+        q.as_mut_ptr() as *mut _,
+        ldq,
+        work.as_mut_ptr() as *mut _,
+        rwork.as_mut_ptr(),
+        info,
+    )
+}
